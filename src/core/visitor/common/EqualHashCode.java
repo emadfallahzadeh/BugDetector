44311f2c0647b022b437f48fb8de38c08391c5b4
emadfallahzadeh <emadfallahzadeh@172.31.47.140>
2019-10-06 15:29:37 -0400

    EqualHashCode added

---
+++ src/core/visitor/common/EqualHashCode.java
@@ -0,0 +1,66 @@
+package core.visitor.common;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.eclipse.jdt.core.ICompilationUnit;
+import org.eclipse.jdt.core.dom.ASTVisitor;
+import org.eclipse.jdt.core.dom.CompilationUnit;
+import org.eclipse.jdt.core.dom.IVariableBinding;
+import org.eclipse.jdt.core.dom.MethodDeclaration;
+import antipattern.detection.run.Application;
+
+import org.eclipse.jdt.core.IPackageFragment;
+
+
+import core.datastructure.impl.Method;
+
+import core.helper.ObjectCreationHelper;
+
+public class EqualHashCode extends ASTVisitor {
+	
+	
+	private String sysName = "cloudstack"; //Name of the project
+	private String className = "";
+	private CompilationUnit parsedunit;
+	public Application app;
+	public boolean hasEqual=false;
+	public boolean hasHashcode=false;
+	public String str;
+		
+	public Map<IVariableBinding, VariableTrack> booleanVariablesMap = new HashMap<IVariableBinding, VariableTrack>();
+		
+	public EqualHashCode(IPackageFragment packageFrag, ICompilationUnit unit, CompilationUnit parsedunit,Application app ) {
+		
+		this.app=app;
+
+		className = unit.getElementName().split("\\.")[0];
+		this.parsedunit = parsedunit;
+	}
+	
+	public boolean visit(MethodDeclaration method) {
+
+		final Method callsite = ObjectCreationHelper.createMethodFromMethodDeclaration(method, className);
+		
+		String ar=method.getName().toString();
+		if(ar.equals("equals")) {
+			hasEqual=true;
+			int lineNumber = parsedunit.getLineNumber(method.getStartPosition());
+
+			 str = "<system>" + sysName + "</system>" + "<callsite>" + callsite
+					+ "</callsite>" + "<line>" + lineNumber + "</line>";
+		}
+		
+		if(ar.equals("hashCode")) {
+			hasHashcode=true;			
+		}		
+		return false;
+	}
+
+
+	
+	
+
+	
+
+}
